31df40b40216078c4828943e199eac2feb391f5e
Enol <grimgaiden@gmail.com>
2020-07-16 11:09:01 +0200

    Added validation to form

--- src/main/java/com/bolsadeideas/springboot/app/springbootdatajpa/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/springbootdatajpa/controllers/ClienteController.java
@@ -2,12 +2,15 @@ package com.bolsadeideas.springboot.app.springbootdatajpa.controllers;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import com.bolsadeideas.springboot.app.springbootdatajpa.models.dao.ClienteDao;
 import com.bolsadeideas.springboot.app.springbootdatajpa.models.entity.Cliente;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -33,8 +36,16 @@ public class ClienteController {
         return "form";
     }
 
+    //Para pasar al formulario el objeto cliente al haber error tienen que coincider el nombre de la clase "Cliente" y el
+    //nombre con el que se utiliza en la vista, si no hay que especificar el nombre utilizado en los atributos del POST con
+    //@ModelAttribute("nombre que se envía a la vista")
     @PostMapping("/form")
-    public String guardar(Cliente cliente) {
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+
+        if(result.hasErrors()) {
+            model.addAttribute("titulo", "Formulario de Cliente");
+            return "form";
+        }
         clienteDao.save(cliente);
         return "redirect:listar";
     }
